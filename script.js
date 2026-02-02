d190ba803285a381a49f846a85a40f4ad3dbfd4c
Tu Nombre <alexgain2000@gmail.com>
2026-02-02 23:39:41 +0100

    Primera modificació

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-// Muestra un saludo por defecto
-document.getElementById("mensaje").textContent = "¡Espero que tengas un buen día!";
+const hora = new Date().getHours();
+let mensaje = "";
+if(hora < 12){
+    mensaje = "¡Buenos días!";
+} else if (hora < 18){
+    mensaje = "¡Buenas tardes!";
+} else {
+    mensaje = "¡Buenas noches!";
+}
+document.getElementById("mensaje").textContent = mensaje;
